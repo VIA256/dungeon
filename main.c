0d6b46ca0dd0baca0f3b58cfdcd67b8176e3f31b
VIA256 <via256@mail.com>
2025-05-29 19:25:33 -0700

    deltatime & fps test

--- main.c
+++ main.c
@@ -1,11 +1,34 @@
-#include <stdio.h>
 #include <raylib.h>
+
+#include <stdio.h>
+#include <errno.h>
+
+
+#define FPS 60
+#define DELTATIME(void) (GetFrameTime()*FPS)
 
 int main(void){
   InitWindow(640, 480, "HELLO");
+  if(!IsWindowReady()){
+    perror("failed to initialize raylib. bye bye.\n");
+    return -1;
+  }
+
+  SetTargetFPS(FPS);
+
+  SetTraceLogLevel(LOG_DEBUG);
+  
   while(!WindowShouldClose()){
     PollInputEvents();
+
+    BeginDrawing();
+    EndDrawing();
+    
+    SwapScreenBuffer();
+
+    printf("DELTATIME = %f\n", DELTATIME());
   }
+  
   CloseWindow();
   
   return 0;
