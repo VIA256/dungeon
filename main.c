e57c32f2459de840a3a770f1e113d8259eabc8e4
VIA256 <via256@mail.com>
2025-05-29 21:30:43 -0700

    camera and blue sky

--- main.c
+++ main.c
@@ -8,6 +8,20 @@
 #define DELTATIME(void) (GetFrameTime()*FPS)
 
 int main(void){
+  Camera3D cam = { 0 };/*(Camera3D){
+			 (Vector3){ 0 },
+			 (Vector3){ 1.0f, 0.0f, 0.0f },
+			 (Vector3){ 0.0f, 1.0f, 0.0f },
+			 80.0f,
+			 CAMERA_PERSPECTIVE
+			 };*/
+  cam.position = (Vector3){ 0 };
+  cam.target = (Vector3){ 1.0f, 0.0f, 0.0f };
+  cam.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+  cam.fovy = 80.0f;
+  cam.projection = CAMERA_PERSPECTIVE;
+
+  
   InitWindow(640, 480, "HELLO");
   if(!IsWindowReady()){
     perror("failed to initialize raylib. bye bye.\n");
@@ -20,13 +34,13 @@ int main(void){
   
   while(!WindowShouldClose()){
     PollInputEvents();
+    UpdateCamera(&cam, CAMERA_FIRST_PERSON);
 
+    ClearBackground(SKYBLUE);
     BeginDrawing();
     EndDrawing();
     
     SwapScreenBuffer();
-
-    printf("DELTATIME = %f\n", DELTATIME());
   }
   
   CloseWindow();
